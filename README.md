2519f7108570373483e51b4d424cc40cab8a73c7
Torben Haack <haack@hey.com>
2020-11-01 05:49:37 +0000

    Updated for tagesschau-widget.js

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
+# Scritable.app scripts
+
+## Widgets
+
+### tagesschau
+
+**Description**
+A scriptable widget which displays the latest tagesschau.de article 📰
+
+**Preview**
+
+**Code**
 
--- tagesschau-widget/tagesschau-widget.js
+++ tagesschau-widget/tagesschau-widget.js
@@ -1,4 +1,155 @@
 // Variables used by Scriptable.
 // These must be at the very top of the file. Do not edit.
 // icon-color: deep-blue; icon-glyph: newspaper;
-const tagesschauData=await new Request('https://www.tagesschau.de/api2/homepage/').loadJSON();const widget=await createWidget();if(!config.runsInWidget){await widget.presentMedium()}else{Script.setWidget(widget)}Script.complete();async function createWidget(){let w=new ListWidget();w.setPadding(15,15,15,15);w.backgroundImage=await loadImage('https://raw.githubusercontent.com/trbnhck/scriptable-scripts/main/tagesschau-widget/assets/tagesschau-bg.png');const tagesschauLogo=w.addImage(await loadImage('https://github.com/trbnhck/scriptable-scripts/blob/main/tagesschau-widget/assets/tagesschau-logo_white.png?raw=true'));tagesschauLogo.leftAlignImage();tagesschauLogo.imageSize=new Size(100,72);const titleLabel=tagesschauData.news[0].title;const ressortLabel=tagesschauData.news[0].ressort;const date=new Date(tagesschauData.news[0].date);const dateLabel=date.toLocaleDateString()+', '+('0'+date.getHours()).slice(-2)+':'+('0'+date.getMinutes()).slice(-2)+' Uhr';const imageLabel=tagesschauData.news[0].teaserImage.videowebl.imageurl;w=await createArticle(w,titleLabel,ressortLabel,dateLabel,imageLabel);w.url=tagesschauData.news[0].shareURL;return w}async function createArticle(w,title,ressort,date,image){const a=w.addStack();const aImage=a.addImage(await loadImage(image));aImage.cornerRadius=5;a.addSpacer(10);const aInfo=a.addStack();aInfo.layoutVertically();const aRessort=aInfo.addText(ressort.charAt(0).toUpperCase()+ressort.slice(1));aRessort.textColor=Color.orange();aRessort.font=Font.semiboldMonospacedSystemFont(12);const aTitle=aInfo.addText(title);aTitle.lineLimit=4;aTitle.textColor=Color.white();aTitle.font=Font.headline();const aDate=aInfo.addText(date);aDate.textOpacity=0.7;aDate.font=Font.semiboldMonospacedSystemFont(12);return w}async function loadImage(url){return await new Request(url).loadImage()}
+
+// name: tagesschau-widget.js
+// description: A widget which displays the latest tagesschau.de article 📰
+// author: Torben Haack
+// email: haack@hey.com
+
+let tagesschauData;
+try {
+  tagesschauData = await new Request(
+    'https://www.tagesschau.de/api2/homepage/'
+  ).loadJSON();
+} catch (e) {
+  const errorWidget = createErrorWidget();
+
+  if (!config.runsInWidget) {
+    await errorWidget.presentMedium();
+  } else {
+    Script.setWidget(errorWidget);
+  }
+  Script.complete();
+}
+const widget = await createWidget();
+
+if (!config.runsInWidget) {
+  await widget.presentMedium();
+} else {
+  Script.setWidget(widget);
+}
+Script.complete();
+
+async function createWidget() {
+  let listWidget = new ListWidget();
+
+  listWidget.setPadding(15, 15, 15, 15);
+
+  listWidget.backgroundImage = await loadImage(
+    'http://www.tagesschau.de/infoscreen/img/background-16-9-HD.png'
+  );
+
+  listWidget = await createHeaderImage(listWidget);
+
+  listWidget.addSpacer(10);
+
+  listWidget = await createArticle(listWidget, tagesschauData.news[0]);
+
+  return listWidget;
+}
+
+async function createArticle(listWidget, data) {
+  const { title, shareURL } = data;
+  const date = new Date(data.date);
+  const image = data.teaserImage.videowebl.imageurl;
+  let { ressort } = data;
+
+  if (ressort == undefined) {
+    ressort = 'Sonstiges';
+  }
+
+  listWidget.url = shareURL;
+
+  listWidget.addSpacer(20);
+
+  const article = listWidget.addStack();
+
+  const articleImage = article.addImage(await loadImage(image));
+  articleImage.cornerRadius = 5;
+
+  article.addSpacer(10);
+
+  const articleInfo = article.addStack();
+  articleInfo.layoutVertically();
+
+  const articleRessort = articleInfo.addText(
+    ressort.charAt(0).toUpperCase() + ressort.slice(1)
+  );
+  articleRessort.textColor = Color.orange();
+  articleRessort.font = Font.semiboldMonospacedSystemFont(12);
+
+  const articleTitle = articleInfo.addText(title.replaceAll('+', '').trim());
+  articleTitle.textColor = Color.white();
+  articleTitle.font = Font.headline();
+  articleTitle.minimumScaleFactor = 0.5;
+
+  const articleDate = articleInfo.addText(formatDate(date));
+  articleDate.font = Font.semiboldMonospacedSystemFont(12);
+  articleDate.textOpacity = 0.7;
+
+  listWidget.addSpacer(10);
+
+  return listWidget;
+}
+
+async function createHeaderImage(listWidget) {
+  const headerImage = listWidget.addImage(
+    await loadImage(
+      'https://www.ard.de/image/461284/16x9/4788513279306625340/320'
+    )
+  );
+  headerImage.imageSize = new Size(100, 5);
+  headerImage.tintColor = Color.white();
+  headerImage.centerAlignImage();
+  headerImage.applyFillingContentMode();
+
+  return listWidget;
+}
+
+function createErrorWidget() {
+  const errorWidget = new ListWidget();
+
+  const bgGradient = new LinearGradient();
+  bgGradient.locations = [0, 1];
+  bgGradient.colors = [new Color('#2D65AE'), new Color('#19274C')];
+
+  errorWidget.backgroundGradient = bgGradient;
+
+  const title = errorWidget.addText('tagesschau');
+  title.font = Font.headline();
+  title.centerAlignText();
+
+  errorWidget.addSpacer(10);
+
+  const errorText = errorWidget.addText(
+    'Es besteht keine Verbindung zum Internet.'
+  );
+  errorText.font = Font.semiboldMonospacedSystemFont(16);
+  errorText.textColor = Color.red();
+
+  const errorText2 = errorWidget.addText(
+    'Dieses Widget benötigt eine Verbindung zum Internet um funktionieren zu können.'
+  );
+  errorText2.font = Font.regularRoundedSystemFont(14);
+  errorText2.textColor = Color.red();
+  errorText2.textOpacity = 0.6;
+
+  return errorWidget;
+}
+
+async function loadImage(url) {
+  return await new Request(url).loadImage();
+}
+
+function formatDate(dateObject) {
+  return `${leadingZero(dateObject.getDate())}.${leadingZero(
+    dateObject.getMonth() + 1
+  )}.${dateObject.getFullYear()}, ${leadingZero(
+    dateObject.getHours()
+  )}:${leadingZero(dateObject.getMinutes())} Uhr`;
+}
+
+function leadingZero(input) {
+  return ('0' + input).slice(-2);
+}
